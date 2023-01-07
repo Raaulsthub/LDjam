767763a190f9276987407eba037a7621c9742f71
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-01-06 22:12:18 -0300

    nothing really

---
+++ Assets/Scripts/Player/PlayerInfo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+    [SerializeField] private int hp;
+    [SerializeField] private int cash;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
