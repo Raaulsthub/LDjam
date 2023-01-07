1d57b69ec6992caf2c5b3a39ee2501bf25c7454d
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-01-07 00:31:51 -0300

    mouse tracing

--- Assets/Scripts/General/InputController.cs
+++ Assets/Scripts/General/InputController.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour
 {
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        this.mainCamera = FindObjectOfType<Camera>();
+    }
     private void Update()
     {
-        Debug.Log(Input.mousePosition);
+        Debug.Log(mainCamera.ScreenToWorldPoint(Input.mousePosition));
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0;
+        transform.position = mouseWorldPosition;    
     }
 }
